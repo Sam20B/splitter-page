0433c17def367673f947eccc9d72fe3d737e7ce7
Sam20B <wafulabibiana93@gmail.com>
2025-08-10 19:01:31 +0300

    bug fixing

--- splitterPage.js
+++ splitterPage.js
@@ -116,7 +116,7 @@ function multiplyValues2() {
     }*/
 
     // Multiply the values
-    const result = input1Value * input2Value;
+    //const result = input1Value * input2Value;
 
     // Display the result
     const input4 = document.getElementsByClassName('c2p7')[0];
@@ -140,8 +140,8 @@ function multiplyValues2() {
     }
 
     // Multiply the values ... add the whole bill instead of multiplying the bill by the number of people again
-    const answer = billInputValue * peopleNumberValue /* * peopleNumberValue*/;
-    const total = result + answer;
+    const answer = input1Value * peopleNumberValue /* * peopleNumberValue*/;
+    const total = input2Value + answer;
 
     // Display the result
     const input5 = document.getElementsByClassName('c2p8')[0];
